0dd09177c468d5f2d0aca8c5acfb2ee3e56fef30
Joyonto <joyonto.jatri@gmail.com>
2021-06-03 15:20:41 +0600

    react bootstrap updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div class="container">
-      <div className="shadow">
+      <div className="shadow p-5 rounded">
         <h1>Hi, This is React-Bootstrap</h1>
+        <Button variant="primary">Hi i am a button</Button>
       </div>
     </div>
   );
